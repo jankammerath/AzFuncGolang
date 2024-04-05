6dbcfe198335738febf3d98f3c09ffd96cb48b30
Jan Kammerath <jankammerath@gmail.com>
2024-04-05 23:59:14 +0200

    added cpuinfo method

--- run.go
+++ run.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/shirou/gopsutil/cpu"
 )
 
 func get_port() string {
@@ -15,12 +16,30 @@ func get_port() string {
 	return port
 }
 
+func getCPUName() string {
+	info, err := cpu.Info()
+	if err != nil {
+		return err.Error()
+	}
+	if len(info) > 0 {
+		return info[0].ModelName
+	}
+	return ""
+}
+
 func main() {
 	router := gin.Default()
 
 	router.GET("/api/hello", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "Hello, World!",
+		})
+	})
+
+	router.GET("/api/cpuinfo", func(c *gin.Context) {
+		cpuName := getCPUName()
+		c.JSON(http.StatusOK, gin.H{
+			"cpu": cpuName,
 		})
 	})
 
